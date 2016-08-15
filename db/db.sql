47650cd02a327678090e05f4959c7344e5829304
zhangkexiang <59916375@qq.com>
2016-08-15 14:11:18 +0800

    testcode

---
+++ db/db.sql
@@ -0,0 +1,71 @@
+-- 分类和软件关系表
+
+DROP TABLE IF EXISTS `categorys2softs`;
+CREATE TABLE `categorys2softs` (
+  `categoryno` varchar(11) NOT NULL DEFAULT '' COMMENT '分类标识',
+  `softunique` varchar(11) NOT NULL DEFAULT '' COMMENT '软件唯一标识'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+
+-- 分类表
+
+DROP TABLE IF EXISTS `categorys`;
+CREATE TABLE `categorys` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `no` varchar(11) NOT NULL DEFAULT '' COMMENT '分类编号',
+  `name` varchar(11) NOT NULL DEFAULT '' COMMENT '分类名称',
+  `parentno` varchar(11) NOT NULL DEFAULT '' COMMENT '父级编号',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+--
+
+
+-- 评论表
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL DEFAULT '' COMMENT '评论人名称',
+  `commentdate` date NOT NULL COMMENT '评论时间',
+  `comment` varchar(500) NOT NULL DEFAULT '' COMMENT '评论内容',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+--
+
+-- 软件表
+DROP TABLE IF EXISTS `softs`;
+CREATE TABLE `softs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `softunique` varchar(50) NOT NULL DEFAULT '' COMMENT '软唯一标识',
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '名称',
+  `downloadtimes` int(11) NOT NULL DEFAULT '0' COMMENT '下载次数',
+  `suporttimes` int(11) NOT NULL DEFAULT '0' COMMENT '喜欢人数',
+  `commenttimes` int(11) NOT NULL DEFAULT '0' COMMENT '评论次数',
+  `note` varchar(200) NOT NULL DEFAULT '' COMMENT '描述',
+  `imgs` varchar(1000) NOT NULL DEFAULT '' COMMENT '截图',
+  `downloadlink` varchar(300) NOT NULL DEFAULT '' COMMENT '下载链接',
+  `updatenote` varchar(200) NOT NULL DEFAULT '' COMMENT '更新内容',
+  `version` varchar(200) NOT NULL DEFAULT '' COMMENT '版本',
+  `size` int(11) NOT NULL DEFAULT '0' COMMENT '大小',
+  `categorynos` varchar(400) NOT NULL DEFAULT '' COMMENT '分类',
+  `tag` varchar(500) NOT NULL DEFAULT '' COMMENT '标签',
+  `updatetimes` varchar(12) NOT NULL COMMENT '更新时间',
+  `request` varchar(200) NOT NULL DEFAULT '' COMMENT '权限要求',
+  `createfrom` varchar(300) NOT NULL DEFAULT '' COMMENT '生产厂家',
+  `erweimatupian` varchar(200) NOT NULL DEFAULT '' COMMENT '二维码图片',
+  `recommends` varchar(400) NOT NULL DEFAULT '' COMMENT '相关推荐',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+--
+
+-- 抓取链接的表
+DROP TABLE IF EXISTS `urls`;
+CREATE TABLE `urls` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(300) NOT NULL DEFAULT '',
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT '状态0-未抓取 1-已抓取',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+)  ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
